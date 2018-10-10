158db444edbd035b5ab54a13ad735335e0bd120b
Alexander Elgin <ostelaymetaule@gmail.com>
2018-10-10 19:26:49 +0200

    Added viewComponent example

---
+++ src/dotnet_sandbox.mvc/ViewComponents/FirstViewComponent.cs
@@ -0,0 +1,58 @@
+﻿using dotnet_sandbox.mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_sandbox.mvc.ViewComponents
+{
+    public class FirstViewComponent : ViewComponent
+    {
+
+        /// <summary>
+        /// This method is called from the main view
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await GetItems();
+            return View(items);
+        }
+
+        /// <summary>
+        /// Generating some data using the same data classes as in another example
+        /// </summary>
+        /// <returns></returns>
+        private Task<List<ListItemViewModel>> GetItems()
+        {
+            return Task.Run(() =>
+            {
+                Task.Delay(1000);
+                return new List<ListItemViewModel>()
+                {
+                    new ListItemViewModel()
+                    {
+                        Index=1,
+                        Name = "Another first item",
+                        Time= DateTime.Now.AddHours(-5).AddYears(5)
+                    },
+                     new ListItemViewModel()
+                    {
+                        Index=2,
+                        Name = "Another second item",
+                        Time= DateTime.Now.AddHours(-3).AddDays(3)
+                    }
+                     ,
+                     new ListItemViewModel()
+                    {
+                        Index=2,
+                        Name = "Another third item",
+                        Time= DateTime.Now.AddHours(7).AddDays(222)
+                    }
+                };
+            });
+
+        }
+    }
+}
---
+++ src/dotnet_sandbox.mvc/ViewComponents/SecondViewComponent.cs
@@ -0,0 +1,62 @@
+﻿using dotnet_sandbox.mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_sandbox.mvc.ViewComponents
+{
+    public class SecondViewComponent : ViewComponent
+    {
+        /// <summary>
+        /// This method is called from the main view
+        /// </summary>
+        /// <param name="elementCount">Number of the data entries should be generated for the test</param>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync(int elementCount)
+        {
+            //Use non standard view name. You can define multiple views and choise one of them this way
+            string viewName = "SecondNotDefaultView";
+            //pass the parameter via temp data to the view
+            this.TempData.Add("myparameter", elementCount);
+
+            var items = await GetItems(elementCount);
+            return View(viewName, items);
+        }
+
+        /// <summary>
+        /// generating the dataset
+        /// </summary>
+        /// <param name="elementCount">Number of the data entries should be generated for the test</param>
+        /// <returns></returns>
+        private Task<List<OtherItemViewModel>> GetItems(int elementCount)
+        {
+            return Task.Run(() =>
+            {
+                Task.Delay(1000);
+                var list = new List<OtherItemViewModel>();
+                for (int i = 1; i < elementCount + 1; i++)
+                {
+                    Task.Delay(100);
+
+                    list.Add(new OtherItemViewModel()
+                    {
+                        Guid = Guid.NewGuid(),
+                        //create some double values
+                        Value = (double)i + (double)i / 7.1
+                    });
+
+                }
+                return list;
+            }
+            );
+        }
+
+
+
+
+
+
+    }
+}
